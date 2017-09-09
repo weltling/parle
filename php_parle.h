74a40da053f6754141b2a69614237865494d4fcf
Anatol Belski <ab@php.net>
2017-09-09 17:09:52 +0200

    Yet one release today

--- php_parle.h
+++ php_parle.h
@@ -35,7 +35,7 @@
 extern zend_module_entry parle_module_entry;
 #define phpext_parle_ptr &parle_module_entry
 
-#define PHP_PARLE_VERSION "0.5.3-dev"
+#define PHP_PARLE_VERSION "0.5.3"
 
 #ifdef PHP_WIN32
 #	define PHP_PARLE_API __declspec(dllexport)
