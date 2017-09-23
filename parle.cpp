9e8c608ee782666dff176f85aeabc1e7774df038
Anatol Belski <ab@php.net>
2017-09-23 19:10:27 +0200

    Revamp errorInfo method

--- parle.cpp
+++ parle.cpp
@@ -942,17 +942,15 @@ PHP_METHOD(ParleParser, errorInfo)
 	try {
 		add_property_long_ex(return_value, "id", sizeof("id")-1, static_cast<zend_long>(par.results.entry.param));
 		add_property_long_ex(return_value, "position", sizeof("position")-1, static_cast<zend_long>(lex.iter->first - lex.in.begin()));
-		if (par.results.entry.param == parsertl::unknown_token) {
-			zval token;
-			std::string ret = lex.iter->str();
-			object_init_ex(&token, ParleToken_ce);
+		zval token;
+		std::string ret = lex.iter->str();
+		object_init_ex(&token, ParleToken_ce);
 #if PHP_MAJOR_VERSION > 7 || PHP_MAJOR_VERSION >= 7 && PHP_MINOR_VERSION >= 2
-			add_property_stringl_ex(&token, "value", sizeof("value")-1, ret.c_str(), ret.size());
+		add_property_stringl_ex(&token, "value", sizeof("value")-1, ret.c_str(), ret.size());
 #else
-			add_property_stringl_ex(&token, "value", sizeof("value")-1, (char *)ret.c_str(), ret.size());
+		add_property_stringl_ex(&token, "value", sizeof("value")-1, (char *)ret.c_str(), ret.size());
 #endif
-			add_property_zval_ex(return_value, "token", sizeof("token")-1, &token);
-		}
+		add_property_zval_ex(return_value, "token", sizeof("token")-1, &token);
 		/* TODO provide details also for other error types, if possible. */
 	} catch (const std::exception &e) {
 		zend_throw_exception(ParleLexerException_ce, e.what(), 0);
