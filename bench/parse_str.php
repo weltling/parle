92b4423815e785ccc694f20bf3b4f8bb08a8a81e
Anatol Belski <ab@php.net>
2017-09-25 17:14:56 +0200

    Add also bench with random input for parse_str bench

--- bench/parse_str.php
+++ bench/parse_str.php
@@ -26,4 +26,47 @@ echo 'Took ', (($endTime - $startTime) * 1000), ' milliseconds with native parse
 //var_dump($out0);
 //var_dump($out1);
 
+/* Check same with the random input. */
+
+function rand_in(bool $for_key = false, $len = -1)
+{
+	$len = $len < 0 ? rand(8, 16) : $len;
+
+	$src = range("a", "z");
+	$src = array_merge($src, range(0, 9));
+	if (!$for_key) {
+		$src = array_merge($src, array("+", "-"));
+	}
+
+	$ret = "";
+	for ($i = 0; $i < $len; ++$i) {
+	    $ret .= $src[mt_rand(0, count($src) - 1)];
+	}
+
+	return $ret;
+}
+
+$in = rand_in(true) . "[" . rand_in(true) . "][][]=" . rand_in()
+	. "&" . rand_in(true) . "[][]=" . rand_in()
+	. "&" . rand_in(true) . "[" . rand_in(true) . "][]=" . rand_in()
+        . "&" . rand_in(true) . "=" . rand_in()
+	. "&" . rand_in(true) . "[]=" . rand_in()
+	. "&" . rand_in(true) . "=";
+$in = rtrim(str_repeat($in . "&", 160), "&");
+
+$out0 = array();
+$startTime = microtime(true);
+Parle\parse_str($in, $out0);
+$endTime = microtime(true);
+echo 'Took ', (($endTime - $startTime) * 1000), ' milliseconds on random input with parle', "\n";
+
+$out1 = array();
+$startTime = microtime(true);
+\parse_str($in, $out1);
+$endTime = microtime(true);
+echo 'Took ', (($endTime - $startTime) * 1000), ' milliseconds on random input with native parse_str', "\n";
+
+//var_dump($out0);
+//var_dump($out1);
+
 
