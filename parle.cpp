091ea8ddfb31db0f7267bfdcfaaad3778cf3be53
Anatol Belski <ab@php.net>
2017-11-04 18:35:16 +0100

    Wrap code that can throw exception

--- parle.cpp
+++ parle.cpp
@@ -552,8 +552,12 @@ _lexer_reset(INTERNAL_FUNCTION_PARAMETERS, zend_class_entry *ce) noexcept
 		return;
 	}
 
-	// TODO check the behavior when inside parser
-	lex.iter.reset(lex.in.begin() + pos, lex.in.begin() + pos);
+	try {
+		// TODO check the behavior when inside parser
+		lex.iter.reset(lex.in.begin() + pos, lex.in.begin() + pos);
+	} catch (const std::exception &e) {
+		php_parle_rethrow_from_cpp(ParleLexerException_ce, e.what(), 0);
+	}
 }/*}}}*/
 
 /* {{{ public void Lexer::reset(int $position) */
