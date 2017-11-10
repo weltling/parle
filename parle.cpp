5c42f9fa3b4ba3626f7e636b890f67df169f2658
Anatol Belski <ab@php.net>
2017-11-10 23:06:36 +0100

    Add minfo line about UTF-32

--- parle.cpp
+++ parle.cpp
@@ -2640,6 +2640,7 @@ PHP_MINFO_FUNCTION(parle)
 	php_info_print_table_start();
 	php_info_print_table_header(2, "Lexing and parsing support", "enabled");
 	php_info_print_table_row(2, "Parle version", PHP_PARLE_VERSION);
+	php_info_print_table_row(2, "Parle internal UTF-32", (PARLE_U32 ? "yes" : "no"));
 	php_info_print_table_end();
 
 	/* Remove comments if you have entries in php.ini
