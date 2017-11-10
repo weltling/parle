478532ffe22e31272063b40c941891d178dcd9fe
Anatol Belski <ab@php.net>
2017-11-10 23:50:25 +0100

    Seems GCC sees __thread and thred_local somewhat different

--- parle.cpp
+++ parle.cpp
@@ -105,7 +105,11 @@ namespace parle {/*{{{*/
 #include <locale>
 #include <codecvt>
 
+#if defined(_MSC_VER)
 ZEND_TLS std::wstring_convert<std::codecvt_utf8<parle::char_type>, parle::char_type> cvt;
+#else
+static thread_local std::wstring_convert<std::codecvt_utf8<parle::char_type>, parle::char_type> cvt;
+#endif
 #define PARLE_CVT_U32(sptr) cvt.from_bytes(sptr).c_str()
 #define PARLE_SCVT_U32(s) cvt.from_bytes(s)
 #if defined(_MSC_VER)
